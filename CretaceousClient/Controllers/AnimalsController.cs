cf66cf13bf22ec98bd8717bd7a57123772b73554
Bai Jaitrong <bjaitrong@comcast.net>
2023-02-03 11:22:42 -0700

    Add MVC  scaffolding, Animal model, controller, and home page.

---
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using CretaceousClient.Models;
+
+namespace CretaceousClient.Controllers;
+
+public class AnimalsController: Controller
+{
+  public IActionResult Index()
+  {
+    List<Animal> animals = Animal.GetAnimals();
+
+    return View(animals);
+  }
+}
